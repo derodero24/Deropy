21e24303d99877cce8bb4978e5d886832604e671
derodero24 <k.itsh20524abc@gmail.com>
2019-01-19 23:00:36 +0900

    visual.py close #4

--- visual.py
+++ visual.py
@@ -3,12 +3,14 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from PIL import Image
 
 import cv2
 import Deropy.common as cmn
 
 
 def pil2cv(image):
+    ''' PIL型 -> OpenCV型 '''
     new_image = np.array(image)
     if new_image.ndim == 2:  # モノクロ
         pass
@@ -19,8 +21,21 @@ def pil2cv(image):
     return new_image
 
 
+def cv2pil(image):
+    ''' OpenCV型 -> PIL型 '''
+    new_image = image
+    if new_image.ndim == 2:  # モノクロ
+        pass
+    elif new_image.shape[2] == 3:  # カラー
+        new_image = new_image[:, :, ::-1]
+    elif new_image.shape[2] == 4:  # 透過
+        new_image = new_image[:, :, [2, 1, 0, 3]]
+    new_image = Image.fromarray(new_image)
+    return new_image
+
+
 def show_image(image, wait_time=0):
-    ''' 画像表示 (縦,横,チャンネル) '''
+    ''' 画像表示 '''
     if type(image) != np.ndarray:
         image = pil2cv(image)
     cv2.namedWindow('window')
